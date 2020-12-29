0ead05852264bb9c804765d39c23cb9b61b99c9a
jaime.hernandez <jaime.hernandez@bi-geek.com>
2020-12-29 20:11:06 +0100

    Added delete eolic plant from city

--- server/routes/plantRoute.js
+++ server/routes/plantRoute.js
@@ -1,6 +1,7 @@
 module.exports = (app, plant) => {
-    const INTERNAL_ERROR = "An internal error has occurred";
+    const INTERNAL_ERROR = "An internal error has occurred. ";
 
+    // CREATE
     app.post("/api/eoloplants/", async(req, res) => {
         console.log("Creating a new plant");
         try {
@@ -13,8 +14,9 @@ module.exports = (app, plant) => {
         }
     });
 
+    // FIND ALL
     app.get("/api/eoloplants/", async(req, res) => {
-        console.log("Getting all ");
+        console.log("Getting all plants");
         try {
             let plants = await plant.findAll();
             res.status(200).json(plants);
@@ -23,4 +25,17 @@ module.exports = (app, plant) => {
             res.status(500).send(INTERNAL_ERROR + e);
         }
     });
+
+    // DELETE BY CITY
+    app.delete("/api/eoloplants/:city", async(req, res) => {
+        try {
+            console.log("Deleting eolic plant from city: " + req.params.city);
+            let plantDeleted = await plant.destroy({where: {city: req.params.city}});
+            res.status(200).json(plantDeleted);
+        } catch (e) {
+            console.error(e);
+            res.status(500).send(INTERNAL_ERROR +  e);
+        }
+    });
+
 }
